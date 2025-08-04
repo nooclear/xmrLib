ea990ab24d71767c7f7356781d9321f8ac153b88
nooclear <nooclear@protonmail.com>
2025-08-04 12:04:06 -0400

    added get_default_fee_priority

---
+++ get_default_fee_priority.go
@@ -0,0 +1,33 @@
+package xmrLib
+
+import (
+	"errors"
+	"github.com/nooclear/jrpcLib"
+	"io"
+)
+
+func (wallet *Wallet) GetDefaultFeePriority(id string) ([]byte, error) {
+	if res, err := wallet.Call(
+		&jrpcLib.JRPC{
+			Version: "2.0",
+			ID:      id,
+			Method:  "get_default_fee_priority",
+			Params:  nil,
+		}); err != nil {
+		return nil, err
+	} else {
+		defer func() {
+			err = res.Body.Close() // need to find a way to properly handle these errors
+		}()
+		switch res.StatusCode {
+		case 200:
+			if data, err := io.ReadAll(res.Body); err != nil {
+				return nil, err
+			} else {
+				return data, nil
+			}
+		default:
+			return nil, errors.New(res.Status)
+		}
+	}
+}
